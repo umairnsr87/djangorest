c59a85cab7e43a63d90ebd04b945681008b36b68
umair ansari <umairnsr87@gmail.com>
2019-11-17 13:24:17 +0530

    urls in app added for api request calls.

--- djangoapi/myapi/urls.py
+++ djangoapi/myapi/urls.py
@@ -1,6 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+
+router=routers.DefaultRouter()
+router.register('myapi',views.approvalview)
 
 urlpatterns = [
+    path('form/',views.myform,name='myform'),
+    path('api/',include(router.urls)),
+    path('status/',views.approvereject)
+
+
 ]
--- djangoapi/myapi/views.py
+++ djangoapi/myapi/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
-#from sklearn
-
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.core import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import data_for_verification
+from .serializers import data_for_verification_serializer
+import pickle
+import json
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.externals import joblib
+#import .form import MyForm
 
 
 # Create your views here.
+#creating class based view here
+class approvalview(viewsets.ModelViewSet):
+    #getting all the queryset
+    queryset =data_for_verification.objects.all()
+    #getting the serializer class
+    serializer_class = data_for_verification_serializer
+
+
+
+def myform(request):
+    if(request.method=="POST"):
+        form=MyForm(request.POST)
+        if (form.is_valid()):
+            myform=form.save(commit=False)
+        else:
+            form=MyForm()
+
+
+@api_view(["POST"])
+def approvereject(request):
+    try:
+        mdl=joblib.load(r"C:\Users\umairansari\Pictures\Downloads\census_income.pkl")
+        mydata=request.data
+        unit=np.array(list(mydata.values()))
+        unit=unit.reshape(1,-1)
+        scalars=joblib.load("")
+
+    except:
+        print("now values")
+
